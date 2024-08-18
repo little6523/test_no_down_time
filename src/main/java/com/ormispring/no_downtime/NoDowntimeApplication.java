8c388a7e28693ee2bdde7ab700f66ead65b76ccb
hyeonjun.lee <little6523@naver.com>
2024-08-19 01:50:00 +0900

    최초 커밋

---
+++ src/main/java/com/ormispring/no_downtime/NoDowntimeApplication.java
@@ -0,0 +1,13 @@
+package com.ormispring.no_downtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoDowntimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoDowntimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ormispring/no_downtime/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ormispring.no_downtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
